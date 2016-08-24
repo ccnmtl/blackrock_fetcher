e00b6eb595c4eaf8701fe4d32cae3d20af4de371
Nik Nyby <nnyby@columbia.edu>
2016-08-24 16:18:14 -0400

    add env data processor

--- blackrock_data_processor.py
+++ blackrock_data_processor.py
@@ -68,10 +68,7 @@ def process_dendrometer_data(path, filename):
     #   "Red_Oak_4_AVG", "Red_Oak_4_MAX", "Red_Oak_4_MIN", "Red_Oak_4_STD",
     #   "Red_Oak_5_AVG", "Red_Oak_5_MAX", "Red_Oak_5_MIN", "Red_Oak_5_STD"
     #
-    # Here's the columns we want to filter to:
-    #   TIMESTAMP, Red_Oak_1_AVG, Red_Oak_2_AVG, Red_Oak_3_AVG,
-    #   Red_Oak_4_AVG, Red_Oak_5_AVG
-    #
+    # Here are the columns we want to filter to:
     keep_columns = [
         'TIMESTAMP', 'Red_Oak_1_AVG', 'Red_Oak_2_AVG', 'Red_Oak_3_AVG',
         'Red_Oak_4_AVG', 'Red_Oak_5_AVG'
@@ -101,6 +98,51 @@ def process_dendrometer_data(path, filename):
     print('Wrote to %s' % outfile)
 
 
+def process_environmental_data(path, filename):
+    fname = os.path.join(path, filename)
+    rows = []
+    with open(fname, 'r') as csvfile:
+        reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+        for row in reader:
+            rows += [row]
+
+    # Remove blank rows
+    # The incoming CSV contains a few rows near the beginning that we don't
+    # want.
+    del rows[0]
+    del rows[1]
+    del rows[1]
+
+    #
+    # The incoming CSV's header row looks like this:
+    # "TIMESTAMP", "RECORD", "AvgTEMP_C", "MinTEMP_C", "MaxTEMP_C",
+    # "AvgRh", "MaxRh", "MinRh", "AvgVP", "AvgDewPt", "TotalPAR",
+    # "AvgGSR", "AvgWspd", "AvgWdir", "StdDevWdir", "MaxWspd",
+    # "TotalRain", "AvgBP", "MaxBP", "MinBp", "AvgST_10", "AvgST_100",
+    # "MinBatt", "AvgCO2", "AvgOzone", "DSTEMPF", "DSDEPTH", "DSRETRIES",
+    # "SoilM_5cm", "SoilM_15cm", "UVB", "UVTEMP", "UVA", "V2mV", "V2mV2",
+    # "Snow1_BATT", "Snow1_PNLTMP", "CM3_up", "CM3_dn", "CG3_up",
+    #  "CG3_dn", "CM_TempC", "CM3_TempK", "Net_Rs", "Net_R1", "Albedo",
+    # "Up_Total", "Dn_Total", "Net_Total", "AvgPAR_Den", "MaxPAR_Den",
+    # "MinPAR_Den"
+    #
+    # Here are the columns we want to filter to:
+    keep_columns = [
+        'TIMESTAMP', 'AvgTEMP_C', 'AvgVP', 'TotalRain',
+        'SoilM_5cm', 'AvgPAR_Den'
+    ]
+    newrows = filter_columns(keep_columns, rows)
+
+    outfile = os.path.join(PROCESSED_DATA_DIR, filename)
+    with open(outfile, 'w') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+        for row in newrows:
+            writer.writerow(row)
+
+    print('Wrote to %s' % outfile)
+
+
 if __name__ == '__main__':
     path = os.path.join(LOCAL_DIRECTORY_BASE, 'current')
     process_dendrometer_data(path, 'Mnt_Misery_Table20.csv')
+    process_environmental_data(path, 'Lowland.csv')
