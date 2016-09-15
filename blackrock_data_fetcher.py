15f3a8265ec5a227e5cce96e8d9f4610245d628b
Nik Nyby <nnyby@columbia.edu>
2016-09-15 10:45:38 -0400

    Ignore data before september 10th

--- blackrock_data_fetcher.py
+++ blackrock_data_fetcher.py
@@ -124,7 +124,7 @@ def main(argv=None):
     process_dendrometer_data(local_dir, 'Mnt_Misery_Table20.csv')
     process_dendrometer_data(local_dir, 'White_Oak_Table20.csv')
     process_environmental_data(local_dir, 'Lowland.csv',
-                               start_dt=datetime(2016, 9, 1))
+                               start_dt=datetime(2016, 9, 10))
 
     listdir = os.listdir(local_dir)
 
